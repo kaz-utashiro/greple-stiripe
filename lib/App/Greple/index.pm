e5fa260be762ae0bbf9c8a0e20c81caadecded46
Kazumasa Utashiro <kaz@utashiro.com>
2024-10-07 08:39:25 +0900

    minimum update

--- lib/App/Greple/index.pm
+++ lib/App/Greple/index.pm
@@ -1,39 +1,39 @@
 package App::Greple::index;
-use 5.008001;
-use strict;
+
+use 5.024;
 use warnings;
 
 our $VERSION = "0.01";
 
-
-
 1;
-__END__
 
 =encoding utf-8
 
 =head1 NAME
 
-App::Greple::index - It's new $module
+App::Greple::index - Greple index manipulation module
 
 =head1 SYNOPSIS
 
-    use App::Greple::index;
+    greple -Mindex ...
 
 =head1 DESCRIPTION
 
-App::Greple::index is ...
+App::Greple::index is a module for B<greple> to manipulate
+pattern match index.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro
 
 =head1 LICENSE
 
-Copyright (C) Kazumasa Utashiro.
+Copyright ©︎ 2024 Kazumasa Utashiro.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
-=head1 AUTHOR
-
-Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
-
 =cut
+
+__DATA__
 
